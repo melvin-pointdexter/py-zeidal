e8ad9a10c3f03ab44ffd42a3125a7a981352e025
melvin-pointdexter <sagigl@ac.sce.ac.il>
2024-12-30 23:26:08 +0200

    add: function stubs

--- program.py
+++ program.py
@@ -1,5 +1,23 @@
 
+def is_diagonally_dominant(matrix):
+    for index, row in enumerate(matrix):
+        for index, col in enumerate(row):
+
+def shift_to_diagonally_dominant(matrix):
+    
+
+def yahakovi(matrix,sum_vector,variable_vector):
+    
+
+def zeidal(matrix,sum_vector,variable_vector):
+    
+
+def cycler(matrix,sum_vector,variable_vector,cycle,maxcycle):
+    for index, row in enumerate(matrix):
+
 def main():
+    matrixA=[[4,2,0],[2,10,4],[0,4,5]]
+    vectorB=[[2],[6],[5]]
     print("Welcome to py-zeidal!")
 
 main()
