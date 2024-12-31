d1c71ed00dac9be780c115fa9bccc58c2ff0aea5
melvin-pointdexter <sagigl@ac.sce.ac.il>
2024-12-31 20:49:44 +0200

    add: zeidal and yahakovi

--- program.py
+++ program.py
@@ -1,3 +1,5 @@
+import copy
+
 #yahakovi = 1, zeidal = 2
 cycler_type=1
 initial_variable=1
@@ -17,45 +19,52 @@ def shift_to_diagonally_dominant(matrix):
     #stub
 
 def yahakovi(matrix,sum_vector,variable_vector):
-    return
-    #stub
+    variable_archive=list(map(lambda x: initial_variable, variable_vector))
+    for row in range(len(matrix)):
+        curRowSum = - sum_vector[row]
+        for col in range(len(matrix[row])):
+            curRowSum += matrix[row][col] * variable_archive[col] if row != col else 0
+        variable_vector[row] = curRowSum/-matrix[row][row]
 
 def zeidal(matrix,sum_vector,variable_vector):
-    return
-    #stub
+    for row in range(len(matrix)):
+        curRowSum = - sum_vector[row]
+        for col in range(len(matrix[row])):
+            curRowSum += matrix[row][col] * variable_vector[col] if row != col else 0
+        variable_vector[row] = curRowSum/-matrix[row][row]
 
-def cycler(matrix,sum_vector,variable_vector,cycle,maxcycle):
-    variable_vector=map(lambda x: initial_variable, sum_vector)
-    for i in range(1,maxcycle):
+def cycler(matrix,sum_vector,variable_vector,maxcycle):
+    variable_vector=list(map(lambda x: initial_variable, variable_vector))
+    for i in range(maxcycle):
         variable_archive=copy.deepcopy(variable_vector)
         if (cycler_type == 1):
             yahakovi(matrix,sum_vector,variable_vector)
         else:
             zeidal(matrix,sum_vector,variable_vector)
-        flag=true
+        flag=True
         for index in range(len(variable_vector)):
-            flag=flag and abs(variable_vector[index] - variable_archive[index]) <= 0.000001
+            difference = abs(variable_vector[index] - variable_archive[index])
+            flag=flag and difference <= 0.0000001
         if (flag):
-            return variable_vector, true
-    return [], false
-            
-    #stub
+            return variable_vector, True
+    return [], False
 
 def main():
     print("Welcome to py-zeidal!")
-    matrixA=[[4,2,0],[2,10,4],[0,4,5]]
-    vectorB=[2,6,5]
-    vector_variables=[1,1,1]
+    matrixA=[[2,0,1],[1,5,1],[0,3,10]]#[[4,2,0],[2,10,4],[0,4,5]]
+    vectorB=[7,16,56]#[2,6,5]
+    vector_variables=[5,5,5]
     print("Choose algorithm type:\n1 - yahakovi\n2 - zeidal\n")
-    cycler_type=int(input("Enter choice: "))
-    if (is_diagonally_dominant(matrixA)):
+    global cycler_type
+    cycler_type = int(input("Enter choice: "))
+    if (not is_diagonally_dominant(matrixA)):
         print("Not diagonally dominant")
         #check if matrix can still be solveable despite not being diagnoally dominant
-        cycler(matrixA,vectorB,vector_variables,0,14)
+        cycler(matrixA,vectorB,vector_variables,14)
         #check if matrix is solved
         print("Shifting rows to create diagonally dominant")
         shift_to_diagonally_dominant(matrixA)
     print("solving the matrix...")
-    cycler(matrixA,vectorB,vector_variables,0,50)
+    print(cycler(matrixA,vectorB,vector_variables,50))
 
 main()
